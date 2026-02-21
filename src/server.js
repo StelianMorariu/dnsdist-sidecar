420cb17855e4bdf9df324c83ab7f5e2100ad8fec
Stelian Morariu <stelian.morariu@gmail.com>
2026-02-21 15:45:16 +0000

    scaffold

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  if (req.method === 'GET' && url.pathname === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('hello world');
+    return;
+  }
+
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end('Not Found');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
